5e2596cae3b75a3356f11be69c9a02b1db8b7c41
patleo <plsweeney25@gmail.com>
2025-09-23 22:16:42 -0400

    fix: simplify if else statements

--- state/daprstate.go
+++ state/daprstate.go
@@ -2950,18 +2950,27 @@ func (dsm *DaprStateManager) SaveEdgeRecords(edges []*EdgeRecord) error {
 			Str("sql_query", sqlQuery).Str("params", string(jsonData)).Msg("random-walk: adding edge record")
 		if resp, err := (*dsm.client).InvokeBinding(ctx, req); err != nil {
 			if strings.Contains(err.Error(), "invalid header field value") {
+				// if resp == nil {
+				// 	log.Info().Msg("random-walk: response is nil")
+				// } else {
+				// 	if resp.Metadata == nil {
+				// 		log.Info().Msg("random-walk: response.metadata[\"sql\"] does not exist")
+				// 	} else {
+				// 		if val, ok := resp.Metadata["sql"]; ok {
+				// 			log.Info().Str("metadata.sql", val).Msg("random-walk: metadata.sql values")
+				// 		} else {
+				// 			log.Info().Msg("random-walk: response.metadata[\"sql\"] does not exist")
+				// 		}
+				// 	}
+				// }
 				if resp == nil {
 					log.Info().Msg("random-walk: response is nil")
+				} else if resp.Metadata == nil {
+					log.Info().Msg("random-walk: response.metadata[\"sql\"] does not exist")
+				} else if val, ok := resp.Metadata["sql"]; ok {
+					log.Info().Str("metadata.sql", val).Msg("random-walk: metadata.sql values")
 				} else {
-					if resp.Metadata == nil {
-						log.Info().Msg("random-walk: response.metadata[\"sql\"] does not exist")
-					} else {
-						if val, ok := resp.Metadata["sql"]; ok {
-							log.Info().Str("metadata.sql", val).Msg("random-walk: metadata.sql values")
-						} else {
-							log.Info().Msg("random-walk: response.metadata[\"sql\"] does not exist")
-						}
-					}
+					log.Info().Msg("random-walk: response.metadata[\"sql\"] does not exist")
 				}
 				log.Info().Msg("random-walk: ignoring invalid header field value error")
 			} else {
